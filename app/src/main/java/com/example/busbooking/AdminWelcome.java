ce3a8506f4c3fe7c94e23ecd8aa5b5360bd8eb9d
elialuko <114394045+elialuko@users.noreply.github.com>
2024-08-02 20:24:15 +0100

    new

--- app/src/main/java/com/example/busbooking/AdminWelcome.java
+++ app/src/main/java/com/example/busbooking/AdminWelcome.java
@@ -6,10 +6,11 @@ import androidx.cardview.widget.CardView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class AdminWelcome extends AppCompatActivity implements View.OnClickListener {
 
-    CardView analytics, route, logout, analytics2, route2;
+    CardView analytics, route, logout, analytics2, route2, analytics3, route3, bookingprediction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,21 +21,26 @@ public class AdminWelcome extends AppCompatActivity implements View.OnClickListe
         analytics2 = findViewById(R.id.analytics2_card);
         route2 = findViewById(R.id.route2_card);
         logout = findViewById(R.id.logout_card);
+        bookingprediction = findViewById(R.id.logout_card2);
+        analytics3 = findViewById(R.id.analytics3_card);
+        route3 = findViewById(R.id.route3_card);
 
         analytics.setOnClickListener(this);
         route.setOnClickListener(this);
         analytics2.setOnClickListener(this);
         route2.setOnClickListener(this);
         logout.setOnClickListener(this);
+        analytics3.setOnClickListener(this);
+        route3.setOnClickListener(this);
+        bookingprediction.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         Intent i;
         String tag = (String) v.getTag();
-
         switch (tag) {
-            case "analytics":
+                case "analytics":
                 i = new Intent(AdminWelcome.this, ReportAnalytics.class);
                 startActivity(i);
                 break;
@@ -50,9 +56,21 @@ public class AdminWelcome extends AppCompatActivity implements View.OnClickListe
                 i = new Intent(AdminWelcome.this, RouteAnalytics115.class);
                 startActivity(i);
                 break;
-            case "logout" : i = new Intent(AdminWelcome.this, Logout.class);
+                case "logout" :
+                i = new Intent(AdminWelcome.this, Logout.class);
                 startActivity(i);
                 break;
+                case "analytics3":
+                i = new Intent(AdminWelcome.this, FutureStops.class);
+                startActivity(i);
+                break;
+                case "route3":
+                i = new Intent(AdminWelcome.this, FutureBookingCounters.class);
+                startActivity(i);
+                break;
+                case "bookingpredictions":
+                i = new Intent(AdminWelcome.this, BookingPrediction.class);
+                startActivity(i);
         }
     }
 }
